e401dbf8baec582a9dd845238832b75bdc567e1e
Andrew Snyder <Andrewsnydernd@gmail.com>
2021-07-17 00:19:53 -0500

    refactored to use action event

--- background.js
+++ background.js
@@ -1,4 +1,19 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.tabs.executeScript(null, {file: "popup.js"});
+// });
+
+chrome.action.onClicked.addListener((tab) => {
+
+  chrome.scripting.executeScript(
+    {
+      target: {tabId: tab.id},
+      files: ['popup.js'],
+    });
+
+  chrome.tabs.query({
+    active: true,               // Select active tabs
+    lastFocusedWindow: true     // In the current window
+  }, function(array_of_Tabs) {
+    chrome.tabs.sendMessage(tab.id, array_of_Tabs[0].url);
+  });
 });
--- popup.js
+++ popup.js
@@ -1,22 +1,4 @@
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
 
-    // Do NOT forget that the method is ASYNCHRONOUS
-    chrome.tabs.query({
-        active: true,               // Select active tabs
-        lastFocusedWindow: true     // In the current window
-    }, function(array_of_Tabs) {
-      var div = document.createElement("div")
-      var text = document.createTextNode(array_of_Tabs[0].url)
-      div.appendChild(text)
-      document.getElementById("container").appendChild(div)
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  alert(message)
+});
